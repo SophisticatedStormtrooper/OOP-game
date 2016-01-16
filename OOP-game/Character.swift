484f2cb63950d615f9294ba33e9d7970d3818814
ζέυς <Zeus@Michaels-MBP.attlocal.net>
2016-01-15 19:35:13 -0800

    first commit

---
+++ OOP-game/Character.swift
@@ -0,0 +1,49 @@
+//
+//  Character.swift
+//  OOP-game
+//
+//  Created by ζέυς on 1/15/16.
+//  Copyright © 2016 zeus. All rights reserved.
+//
+
+import Foundation
+
+class Character {
+    
+    private var _hp: Int = 100
+    private var _attackPwr: Int = 10
+    
+    var attackPwr: Int {
+        get {
+            return _attackPwr
+        }
+    }
+    
+    var hp: Int {
+        get {
+            return _hp
+        }
+    }
+    
+    var isAlive: Bool {
+        get {
+            if hp <= 0 {
+                return false
+            } else {
+                return true
+            }
+        }
+    }
+    
+    init(startingHp: Int, attackPwr: Int){
+        self._hp = startingHp
+        self._attackPwr = attackPwr
+    }
+    
+    func attemptAttack(attackPwr: Int) -> Bool {
+        self._hp -= attackPwr
+        
+        return true
+    }
+    
+}
